14c60a5a178165e0a0d34675731eda4f7d6ce222
Olek <aleksander.atamanczuk@gmail.com>
2020-07-09 10:52:33 +0200

    New wersion of js conception

--- JS/config/database.js
+++
@@ -1,9 +0,0 @@
-// Create connection
-const mysql = require('mysql');
-
-module.exports = new mysql.createConnection({
-    host: 'localhost',
-    user: 'tengosc',
-    password: 'reja69',
-    database: 'jahl'
-});
