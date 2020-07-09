49dd077f07101ef9b3ee5a662ee47695933000f9
Olek <aleksander.atamanczuk@gmail.com>
2020-07-09 14:43:30 +0200

    Use Traversy Media convention to create project

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Database
+const db = require('./config/database');
+
+// Test DB
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err))
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Database test'))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
